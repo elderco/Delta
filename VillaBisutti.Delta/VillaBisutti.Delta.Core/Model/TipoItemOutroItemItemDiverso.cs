83b8c3f2034077a5a3c7f7db46854e9bf98dff3d
rafarave <rafael.ravena@gmail.com>
2015-06-17 14:30:32 -0300

    Implementig IEntityBase

---
+++ VillaBisutti.Delta/VillaBisutti.Delta.Core/Model/TipoItemOutroItemItemDiverso.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VillaBisutti.Delta.Core.Model
+{
+	class TipoItemOutroItemItemDiverso : IEntityBase
+	{
+		public int Id { get; set; }
+	}
+}
