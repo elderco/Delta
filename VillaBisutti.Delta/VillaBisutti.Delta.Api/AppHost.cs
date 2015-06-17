955bfcbfb13c08a618a60f70a0f826029a93eda2
rafarave <rafael.ravena@gmail.com>
2015-06-17 14:05:57 -0300

    Solution e os Projects adicionados

---
+++ VillaBisutti.Delta/VillaBisutti.Delta.Api/AppHost.cs
@@ -0,0 +1,23 @@
+﻿using ServiceStack.WebHost.Endpoints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VillaBisutti.Delta.ServiceInterface;
+
+namespace VillaBisutti.Delta.Api
+{
+    public class AppHost : AppHostBase
+    {
+        public AppHost()
+            : base("VillaBisutti.Delta.Api", typeof(Class1).Assembly)
+        {
+
+        }
+
+        public override void Configure(Funq.Container container)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VillaBisutti.Delta/VillaBisutti.Delta.Core/Data/Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VillaBisutti.Delta.Core.Data
+{
+	public class Context : DbContext
+	{
+		public Context()
+			: base("VillaBisuttiDelta")
+		{ }
+	}
+}
