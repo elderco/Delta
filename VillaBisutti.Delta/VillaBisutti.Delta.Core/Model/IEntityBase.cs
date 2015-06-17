7425ef2743305e1204c870eed9d18765c4221a26
leal554 <leal_554@msn.com>
2015-06-17 14:13:33 -0300

    Novas Classes mais Interface
    Perfil de usuários do sistema
    Usuários
    Tipo de itens de decoração
    Itens de decoração
    Tipo de itens de montagem
    Itens de montagem
    Tipo de itens de Bolo, doces e bem casado
    Itens de Bolo, doces e bem casado
    Tipo de itens de Gastronomia
    Itens de Gastronomia
    Tipo de itens de Bebidas
    Itens de Bebidas
    Tipo de itens de Som e iluminação
    Itens de Som e iluminação
    Tipo de itens de Foto e vídeo
    Itens de Foto e vídeo
    Tipo de itens de Outros itens / itens diversos
    Itens de Outros itens / itens diversos
    Roteiro padrão
    Cronograma do cerimonial padrão
    Contrato e aditivos
    Decoração
    Montagem
    Bolo, doce e bem casado
    Gastronomia
    Bebidas
    Som e iluminação
    Foto e vídeo
    Outros itens
    Roteiro
    Cronograma do cerimonial
    "appl�i0��1K
    a 3ª linha, por exemplo, será TipoItemDecoracao

---
+++ VillaBisutti.Delta/VillaBisutti.Delta.Core/Model/IEntityBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VillaBisutti.Delta.Core.Model
+{
+	public interface IEntityBase
+	{
+		int Id { get; set; }
+
+
+
+	}
+}
