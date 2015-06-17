bdea76b00ad38a7f044942ed7b8e9cf55b976e36
rafarave <rafael.ravena@gmail.com>
2015-06-17 16:56:51 -0300

    deu ruim

--- VillaBisutti.Delta/VillaBisutti.Delta.Core/Data/Usuario.cs
+++ VillaBisutti.Delta/VillaBisutti.Delta.Core/Data/Usuario.cs
@@ -1,7 +1,6 @@
 ﻿using System.Data.Entity.Infrastructure;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
