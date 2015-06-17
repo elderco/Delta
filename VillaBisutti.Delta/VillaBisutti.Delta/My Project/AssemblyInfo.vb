36197ac016533a8979996e206db0576914f43a10
andredassa <andredassa@hotmail.com>
2015-06-17 14:20:53 -0300

    Novas Classes

--- VillaBisutti.Delta/VillaBisutti.Delta/My Project/AssemblyInfo.vb
+++ VillaBisutti.Delta/VillaBisutti.Delta/My Project/AssemblyInfo.vb
@@ -2,11 +2,19 @@
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
+<<<<<<< HEAD
+' Informações gerais sobre um assembly são controladas através do seguinte 
+' conjunto de atributos. Altere o valor destes atributos para modificar a informação
+' associada a um assembly.
+
+' Revise os valores dos atributos do assembly
+=======
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
 
 ' Review the values of the assembly attributes
+>>>>>>> origin/master
 
 <Assembly: AssemblyTitle("VillaBisutti.Delta")> 
 <Assembly: AssemblyDescription("")> 
@@ -17,6 +25,20 @@ Imports System.Runtime.InteropServices
 
 <Assembly: ComVisible(False)>
 
+<<<<<<< HEAD
+'O GUID a seguir é para o ID da typelib se este projeto for exposto para COM
+<Assembly: Guid("d7c788fb-9596-430c-8c45-7c41ab1d9212")> 
+
+' Informação de versão para um assembly consiste nos quatro valores a seguir:
+'
+'      Versão Principal
+'      Versão Secundária 
+'      Número da Versão
+'      Revisão
+'
+' É possível especificar todos os valores ou usar o padrão de números da Compilação e de Revisão 
+' utilizando o '*' como mostrado abaixo:
+=======
 'The following GUID is for the ID of the typelib if this project is exposed to COM
 <Assembly: Guid("2b4a1ed1-f708-430f-99ac-04c729ef0218")> 
 
@@ -29,6 +51,7 @@ Imports System.Runtime.InteropServices
 '
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
+>>>>>>> origin/master
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("1.0.0.0")> 
