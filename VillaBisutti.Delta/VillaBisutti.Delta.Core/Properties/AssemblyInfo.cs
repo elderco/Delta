df491b1d17d863877957ee8ae2890452979d9694
rafarave <rafael.ravena@gmail.com>
2015-06-17 14:26:38 -0300

    Revert "Novas Classes mais Interface"
    This reverts commit 7425ef2743305e1204c870eed9d18765c4221a26.

--- VillaBisutti.Delta/VillaBisutti.Delta.Core/Properties/AssemblyInfo.cs
+++ VillaBisutti.Delta/VillaBisutti.Delta.Core/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("aca1afa0-7231-44b5-9cec-28d31f99448a")]
+[assembly: Guid("8c5aef35-338e-4b84-ac78-39d8cc723015")]
 
 // Version information for an assembly consists of the following four values:
 //
