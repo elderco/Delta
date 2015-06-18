c0cb09e4060a60225d263cce02da30eb47d19ca5
rafarave <rafael.ravena@gmail.com>
2015-06-18 11:27:53 -0300

    TipoItemBebida que estava faltando

---
+++ VillaBisutti.Delta/VillaBisutti.Delta.Core/Model/TipoItemBebida.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VillaBisutti.Delta.Core.Model
+{
+	public class TipoItemBebida : IEntityBase
+	{
+		public int Id { get; set; }
+	}
+}
