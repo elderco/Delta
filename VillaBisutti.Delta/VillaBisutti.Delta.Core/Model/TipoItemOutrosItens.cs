4507c11ce2bea21d2837f862f4c51e75371593d4
andredassa <andredassa@hotmail.com>
2015-06-24 14:31:05 -0300

    Data Annotations Core.Model
    Adicionando DataAnnotations nas classes do Core.Model

--- VillaBisutti.Delta/VillaBisutti.Delta.Core/Model/TipoItemOutrosItens.cs
+++ VillaBisutti.Delta/VillaBisutti.Delta.Core/Model/TipoItemOutrosItens.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +12,19 @@ namespace VillaBisutti.Delta.Core.Model
 		public int Id { get; set; }
 		public string Nome { get; set; }
 		public List<ItemOutrosItens> Itens { get; set; }
+		[Display(Name = "Item deve ser criado junto com evento do tipo Aniversario")]
+		public bool PadraoAniversario { get; set; }
+		[Display(Name = "Item deve ser criado junto com evento do tipo Barmitzva")]
+		public bool PadraoBarmitzva { get; set; }
+		[Display(Name = "Item deve ser criado junto com evento do tipo Batmitzva")]
+		public bool PadraoBatmitzva { get; set; }
+		[Display(Name = "Item deve ser criado junto com evento do tipo Casamento")]
+		public bool PadraoCasamento { get; set; }
+		[Display(Name = "Item deve ser criado junto com evento do tipo Corporativo")]
+		public bool PadraoCorporativo { get; set; }
+		[Display(Name = "Tipo deve ser criado junto com evento do tipo Debutante")]
+		public bool PadraoDebutante { get; set; }
+		[Display(Name = "Tipo deve ser criado junto com eventro do tipo Outro")]
+		public bool PadraoOutro { get; set; }
 	}
 }
