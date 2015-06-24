fdce7034352a46a0b75adfa28195b33d4d546c9d
rafarave <rafael.ravena@gmail.com>
2015-06-23 21:14:27 -0300

    few

--- VillaBisutti.Delta/VillaBisutti.Delta.WebApp/Core/ServicesFacade.cs
+++ VillaBisutti.Delta/VillaBisutti.Delta.WebApp/Core/ServicesFacade.cs
@@ -14,25 +14,12 @@ namespace VillaBisutti.Delta.WebApp.Core
 				return ConfigurationManager.AppSettings["ApiURL"].ToString();
 			}
 		}
-		//private static HttpClient _client;
-		//private static HttpClient Client
-		//{
-		//	get
-		//	{
-		//		if (_client == null)
-		//		{
-		//			_client = new HttpClient();
-		//			_client.BaseAddress = new Uri(ApiURL);
-		//			_client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-		//		}
-		//		return _client;
-		//	}
-		//}
-		//public List<Delta.Core.Model.TipoItemDecoracao> TiposDecoracao()
-		//{
-		//	HttpResponseMessage response = Client.GetAsync("/tipo-item-bebida");
-			
-		//	return "";
-		//}
+		public static string EventoRoute
+		{
+			get
+			{
+				return ApiURL + "/evento";
+			}
+		}
 	}
 }
