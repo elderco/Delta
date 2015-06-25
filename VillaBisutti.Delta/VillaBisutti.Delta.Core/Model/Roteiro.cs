20ac14da5958f12d981772a9cce4258cace570db
rafarave <rafael.ravena@gmail.com>
2015-06-25 11:52:14 -0300

    Velho velhou
    Código desvelhado no erro errando na relação do roteiro

--- VillaBisutti.Delta/VillaBisutti.Delta.Core/Model/Roteiro.cs
+++ VillaBisutti.Delta/VillaBisutti.Delta.Core/Model/Roteiro.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,8 @@ namespace VillaBisutti.Delta.Core.Model
 	public class Roteiro : IEntityBase
 	{
 		public int Id { get; set; }
+		[Key, ForeignKey("Evento")]
+		public int EventoId { get; set; }
 		public Evento Evento { get; set; }
 		public List<ItemRoteiro> Acontecimentos { get; set; }
 	}
