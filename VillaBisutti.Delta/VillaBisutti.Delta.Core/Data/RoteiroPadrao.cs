bca273f18c1d09dfffe6bdc0327d252f45963d54
leal554 <leal_554@msn.com>
2015-06-25 15:00:24 -0300

    roteiro padrao telas

--- VillaBisutti.Delta/VillaBisutti.Delta.Core/Data/RoteiroPadrao.cs
+++ VillaBisutti.Delta/VillaBisutti.Delta.Core/Data/RoteiroPadrao.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data.Entity.Infrastructure;
+using System.Data.Entity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,7 +30,7 @@ namespace VillaBisutti.Delta.Core.Data
 
 		protected override List<Model.RoteiroPadrao> GetCollection()
 		{
-			return context.RoteiroPadrao.ToList();
+			return context.RoteiroPadrao.Include(id => id.TipoEvento).ToList();
 		}
 	}
 }
