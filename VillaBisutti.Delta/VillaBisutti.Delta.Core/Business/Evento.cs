38279015b3a17856ad4ff4f74f913dc92cf74816
rafarave <rafael.ravena@gmail.com>
2015-06-25 11:05:29 -0300

    Incluido namespace para regras de negócio
    ...Delta.Core.Business

---
+++ VillaBisutti.Delta/VillaBisutti.Delta.Core/Business/Evento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VillaBisutti.Delta.Core.Business
+{
+	public class Evento
+	{
+		public void CopiarRoteiroPadrao(int eventoId)
+		{ }
+	}
+}
