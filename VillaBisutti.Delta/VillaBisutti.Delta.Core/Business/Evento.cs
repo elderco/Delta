3f0c6625f2f23a4b185d6bd2649567fd09a8fdab
andredassa <andredassa@hotmail.com>
2015-06-25 16:53:53 -0300

    Bussiness Evento

--- VillaBisutti.Delta/VillaBisutti.Delta.Core/Business/Evento.cs
+++ VillaBisutti.Delta/VillaBisutti.Delta.Core/Business/Evento.cs
@@ -9,8 +9,20 @@ namespace VillaBisutti.Delta.Core.Business
 	public class Evento
 	{
 		public void CopiarRoteiroPadrao(int eventoId)
-		{ 
+		{
+			//Encontrar o evento que tem este Id passado por parâmetro.
+			Model.Evento evento = new Data.Evento().GetElement(eventoId);
+			//Encontrar o roteiro padrão para o tipo de evento igual ao deste evento
+			Model.RoteiroPadrao roteiro = new Data.RoteiroPadrao().GetCollection(0).FirstOrDefault(rp => rp.TipoEvento == evento.TipoEvento);
+			//passar por cada ItemRoteiro existente no roteiro padrão
 
+
+				//Criar uma nova instância de ItemRoteiro
+				//Atribuit o mesmo nome 
+				//Horário do bicho ficará igual à horário do anterior + horário de início do evento
+				//EventoId do bicho é o Id do evento
+			//adicionar ao context.ItemRoteiro todos estes novos eventos criados
+			//Salvar context
 		}
 	}
 }
