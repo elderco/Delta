6b489a50986d892f0db79813edc27505e06f0097
leal554 <leal_554@msn.com>
2015-06-29 11:25:15 -0300

    modificações de controllers

--- VillaBisutti.Delta/VillaBisutti.Delta.WebApp/Controllers/ItemOutrosItensController.cs
+++ VillaBisutti.Delta/VillaBisutti.Delta.WebApp/Controllers/ItemOutrosItensController.cs
@@ -47,7 +47,7 @@ namespace VillaBisutti.Delta.WebApp.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-		public ActionResult Create([Bind(Include = "Id,Nome,Quantidade,TipoItemOutrosItensId")] model.ItemOutrosItens itemoutrositens)
+		public ActionResult ItemCreated([Bind(Include = "Id,Nome,Quantidade,TipoItemOutrosItensId")] model.ItemOutrosItens itemoutrositens)
         {
             if (ModelState.IsValid)
             {
--- VillaBisutti.Delta/VillaBisutti.Delta.WebApp/Controllers/ItemRoteiroController.cs
+++ VillaBisutti.Delta/VillaBisutti.Delta.WebApp/Controllers/ItemRoteiroController.cs
@@ -47,7 +47,7 @@ namespace VillaBisutti.Delta.WebApp.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-		public ActionResult Create([Bind(Include = "Id,Titulo,Inicio.Hora,Inicio.Minuto,Importante,Observacao,RoteiroPadraoId,RoteiroId")] model.ItemRoteiro itemroteiro)
+		public ActionResult ItemCreated([Bind(Include = "Id,Titulo,Inicio.Hora,Inicio.Minuto,Importante,Observacao,RoteiroPadraoId,RoteiroId")] model.ItemRoteiro itemroteiro)
         {
             if (ModelState.IsValid)
             {
