d135392cfb7bcbc6b7f7a07d8139cc82f2218509
leal554 <leal_554@msn.com>
2015-06-29 12:07:28 -0300

    tipo itens arrumado erros

--- VillaBisutti.Delta/VillaBisutti.Delta.WebApp/Controllers/TipoItemOutrosItensController.cs
+++ VillaBisutti.Delta/VillaBisutti.Delta.WebApp/Controllers/TipoItemOutrosItensController.cs
@@ -46,7 +46,7 @@ namespace VillaBisutti.Delta.WebApp.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include="Id,Nome")] model.TipoItemOutrosItens tipoitemoutrositens)
+		public ActionResult Create([Bind(Include = "Id,Nome,PadraoAniversario,PadraoBarmitzva,PadraoBatmitzva,PadraoCasamento,PadraoCorporativo,PadraoDebutante,PadraoOutro")] model.TipoItemOutrosItens tipoitemoutrositens)
         {
             if (ModelState.IsValid)
             {
@@ -77,7 +77,7 @@ namespace VillaBisutti.Delta.WebApp.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include="Id,Nome")] model.TipoItemOutrosItens tipoitemoutrositens)
+		public ActionResult Edit([Bind(Include = "Id,Nome,PadraoAniversario,PadraoBarmitzva,PadraoBatmitzva,PadraoCasamento,PadraoCorporativo,PadraoDebutante,PadraoOutro")] model.TipoItemOutrosItens tipoitemoutrositens)
         {
             if (ModelState.IsValid)
             {
