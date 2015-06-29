77581945aa24801562a6228b988059da75c28a37
rafarave <rafael.ravena@gmail.com>
2015-06-29 10:57:20 -0300

    Base para editar Itens

--- VillaBisutti.Delta/VillaBisutti.Delta.WebApp/Controllers/TipoItemBebidaController.cs
+++ VillaBisutti.Delta/VillaBisutti.Delta.WebApp/Controllers/TipoItemBebidaController.cs
@@ -46,7 +46,7 @@ namespace VillaBisutti.Delta.WebApp.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include="Id,Nome")] model.TipoItemBebida tipoitembebida)
+		public ActionResult ItemCreated([Bind(Include = "Id,Nome")] model.TipoItemBebida tipoitembebida)
         {
             if (ModelState.IsValid)
             {
