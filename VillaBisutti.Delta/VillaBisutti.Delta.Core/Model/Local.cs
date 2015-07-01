75bf0ae8cf3c593abfae650c2698a2eb7a709842
leal554 <leal_554@msn.com>
2015-07-01 14:04:06 -0300

    telas fixed

--- VillaBisutti.Delta/VillaBisutti.Delta.Core/Model/Local.cs
+++ VillaBisutti.Delta/VillaBisutti.Delta.Core/Model/Local.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace VillaBisutti.Delta.Core.Model
 {
@@ -10,6 +12,7 @@ namespace VillaBisutti.Delta.Core.Model
 	public class Local : IEntityBase
 	{
 		public int Id { get; set; }
+		[Required]
 		public string NomeCasa { get; set; }
 		public string SiglaCasa { get; set; }
 		public string EnderecoCasa { get; set; }
--- VillaBisutti.Delta/VillaBisutti.Delta.Core/Model/TipoItemMontagem.cs
+++ VillaBisutti.Delta/VillaBisutti.Delta.Core/Model/TipoItemMontagem.cs
@@ -10,6 +10,7 @@ namespace VillaBisutti.Delta.Core.Model
 	public class TipoItemMontagem : IEntityBase
 	{
 		public int Id { get; set; }
+		[Required]
 		public string Nome { get; set; }
 		public List<ItemMontagem> Itens { get; set; }
 		[Display(Name= "Item deve ser criado junto com eventos do tipo Aniversário")]
