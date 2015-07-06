df7cfef2a7ccfe7e44cc9796b60095d7f4ddad52
leal554 <leal_554@msn.com>
2015-07-06 16:03:31 -0300

    Model com dataAnotation

--- VillaBisutti.Delta/VillaBisutti.Delta.Core/Model/FornecedorBoloDoceBemCasado.cs
+++ VillaBisutti.Delta/VillaBisutti.Delta.Core/Model/FornecedorBoloDoceBemCasado.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,11 @@ namespace VillaBisutti.Delta.Core.Model
 	public class FornecedorBoloDoceBemCasado : IEntityBase
 	{
 		public int Id { get; set; }
+		[Display(Name = "Nome do fornecedor"), Required]
 		public string NomeFornecedor { get; set; }
+		[Display(Name = "Doces Fornecidos")]
 		public List<ItemBoloDoceBemCasado> DocesFornecidos { get; set; }
+		[Display(Name = "Formas Fornecidos")]
 		public List<ItemFormaBoloDoceBemCasado> FormasFornecidos { get; set; }
 	}
 }
