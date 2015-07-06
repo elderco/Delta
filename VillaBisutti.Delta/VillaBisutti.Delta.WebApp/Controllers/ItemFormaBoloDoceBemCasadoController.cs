60d706c9c383522da7cfca5f667cc03fd424cfe6
andredassa <andredassa@hotmail.com>
2015-07-06 12:22:49 -0300

    ops 2

--- VillaBisutti.Delta/VillaBisutti.Delta.WebApp/Controllers/ItemFormaBoloDoceBemCasadoController.cs
+++ VillaBisutti.Delta/VillaBisutti.Delta.WebApp/Controllers/ItemFormaBoloDoceBemCasadoController.cs
@@ -11,111 +11,111 @@ using data = VillaBisutti.Delta.Core.Data;
 
 namespace VillaBisutti.Delta.WebApp.Controllers
 {
-	public class ItemFormaBoloDoceBemCasado : Controller
-	{
-		// GET: /Default1/
-		public ActionResult Index()
-		{
+	public class ItemFormaBoloDoceBemCasadoController : Controller
+    {
+        // GET: /Default1/
+        public ActionResult Index()
+        {
 			return View(new data.ItemFormaBoloDoceBemCasado().GetCollection(0));
-		}
+        }
 
-		// GET: /Default1/Details/5
-		public ActionResult Details(int? id)
-		{
-			if (id == null)
-			{
-				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-			}
+        // GET: /Default1/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
 			model.ItemFormaBoloDoceBemCasado itemformabolodocebemcasado = new data.ItemFormaBoloDoceBemCasado().GetElement(id.HasValue ? id.Value : 0);
 			if (itemformabolodocebemcasado == null)
-			{
-				return HttpNotFound();
-			}
-			return View(itemformabolodocebemcasado);
-		}
+            {
+                return HttpNotFound();
+            }
+            return View(itemformabolodocebemcasado);
+        }
 
-		// GET: /Default1/Create
-		public ActionResult Create()
-		{
+        // GET: /Default1/Create
+        public ActionResult Create()
+        {
 			SelectList ItemFormaBoloDoceBemCasado = new SelectList(new data.ItemFormaBoloDoceBemCasado().GetCollection(0).OrderBy(tid => tid.Nome), "Id", "Nome");
 			ViewBag.ItemFormaBoloDoceBemCasado = ItemFormaBoloDoceBemCasado;
 			return View();
-		}
+        }
 
-		// POST: /Default1/Create
-		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
+        // POST: /Default1/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
 		public ActionResult ItemCreated([Bind(Include = "Id,Nome,FornecedorId")] model.ItemFormaBoloDoceBemCasado itemformabolodocebemcasado)
-		{
-			if (ModelState.IsValid)
-			{
+        {
+            if (ModelState.IsValid)
+            {
 				new data.ItemFormaBoloDoceBemCasado().Insert(itemformabolodocebemcasado);
-				return RedirectToAction("Index");
-			}
-			return View(itemformabolodocebemcasado);
-		}
+                return RedirectToAction("Index");
+            }
+            return View(itemformabolodocebemcasado);
+        }
 
-		// GET: /Default1/Edit/5
-		public ActionResult Edit(int? id)
-		{
-			if (id == null)
-			{
-				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-			}
+        // GET: /Default1/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
 			model.ItemFormaBoloDoceBemCasado itemformabolodocebemcasado = new data.ItemFormaBoloDoceBemCasado().GetElement(id.HasValue ? id.Value : 0);
-			if (itemformabolodocebemcasado == null)
-			{
-				return HttpNotFound();
-			}
+            if (itemformabolodocebemcasado == null)
+            {
+                return HttpNotFound();
+            }
 			SelectList ItemFormaBoloDoceBemCasado = new SelectList(new data.ItemFormaBoloDoceBemCasado().GetCollection(0).OrderBy(tid => tid.Nome), "Id", "Nome");
 			ViewBag.ItemFormaBoloDoceBemCasado = ItemFormaBoloDoceBemCasado;
 			return View(itemformabolodocebemcasado);
-		}
+        }
 
-		// POST: /Default1/Edit/5
-		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public ActionResult Edit([Bind(Include = "Id,Nome,FornecedorId")] model.ItemFormaBoloDoceBemCasado itemformabolodocebemcasado)
-		{
-			if (ModelState.IsValid)
-			{
+        // POST: /Default1/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,Nome,FornecedorId")] model.ItemFormaBoloDoceBemCasado itemformabolodocebemcasado)
+        {
+            if (ModelState.IsValid)
+            {
 				new data.ItemFormaBoloDoceBemCasado().Update(itemformabolodocebemcasado);
-				return RedirectToAction("Index");
-			}
-			return View(itemformabolodocebemcasado);
-		}
+                return RedirectToAction("Index");
+            }
+            return View(itemformabolodocebemcasado);
+        }
 
-		// GET: /Default1/Delete/5
-		public ActionResult Delete(int? id)
-		{
-			if (id == null)
-			{
-				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-			}
+        // GET: /Default1/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
 			model.ItemFormaBoloDoceBemCasado itemformabolodocebemcasado = new data.ItemFormaBoloDoceBemCasado().GetElement(id.HasValue ? id.Value : 0);
 			if (itemformabolodocebemcasado == null)
-			{
-				return HttpNotFound();
-			}
-			return View(itemformabolodocebemcasado);
-		}
+            {
+                return HttpNotFound();
+            }
+            return View(itemformabolodocebemcasado);
+        }
 
-		// POST: /Default1/Delete/5
-		[HttpPost, ActionName("Delete")]
-		[ValidateAntiForgeryToken]
-		public ActionResult DeleteConfirmed(int id)
-		{
+        // POST: /Default1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
 			new data.ItemFormaBoloDoceBemCasado().Delete(id);
-			return RedirectToAction("Index");
-		}
+            return RedirectToAction("Index");
+        }
 
-		protected override void Dispose(bool disposing)
-		{
-			base.Dispose(disposing);
-		}
-	}
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+    }
 }
