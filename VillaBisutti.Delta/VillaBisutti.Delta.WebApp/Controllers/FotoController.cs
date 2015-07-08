cac7ec5892c7137e187456dfa1aa4335f0e2f41b
rafarave <rafael.ravena@gmail.com>
2015-07-08 18:38:21 -0300

    Foto Upload Matou

--- VillaBisutti.Delta/VillaBisutti.Delta.WebApp/Controllers/FotoController.cs
+++ VillaBisutti.Delta/VillaBisutti.Delta.WebApp/Controllers/FotoController.cs
@@ -12,61 +12,59 @@ using System.IO;
 
 namespace VillaBisutti.Delta.WebApp.Controllers
 {
-    public class FotoController : Controller
-    {
-        // GET: /Foto/
-        public ActionResult Index()
-        {
+	public class FotoController : Controller
+	{
+		// GET: /Foto/
+		public ActionResult Index()
+		{
 			return View(new data.Foto().GetCollection(0));
-        }
+		}
 
-        // GET: /Foto/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+		// GET: /Foto/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
 
-        // POST: /Foto/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
+		// POST: /Foto/Create
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
 		public ActionResult ItemCreated(HttpPostedFileBase URL, string legenda)
-        {
+		{
 			if (URL != null && URL.ContentLength > 0)
 			{
-				var fileName = Util.GetName(URL.FileName);
-				URL.SaveAs(Path.Combine(Server.MapPath("~/Content/TEMP/"), fileName));
-				Util.HandleImage(Path.Combine(Server.MapPath("~/Content/TEMP/"), fileName));
+				string fileName = Util.GetName(URL.FileName);
+				Util.HandleImage(URL, Path.Combine(Server.MapPath("~/Content/Images/"), fileName));
 				model.Foto foto = new model.Foto { Legenda = legenda, URL = fileName };
 				new data.Foto().Insert(foto);
 				SessionFacade.FotoEmMemoria = foto;
-				System.IO.File.Delete(Path.Combine(Server.MapPath("~/Content/TEMP/"), fileName));
 			}
 			return Redirect(Request.UrlReferrer.AbsolutePath);
 		}
 
 
-        // GET: /Foto/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
+		// GET: /Foto/Delete/5
+		public ActionResult Delete(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
 			model.Foto foto = new data.Foto().GetElement(id.HasValue ? id.Value : 0);
 			if (foto == null)
-            {
-                return HttpNotFound();
-            }
-            return View(foto);
-        }
+			{
+				return HttpNotFound();
+			}
+			return View(foto);
+		}
 
-        // POST: /Foto/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
+		// POST: /Foto/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public ActionResult DeleteConfirmed(int id)
+		{
 			//System.IO.File.Delete(Path.Combine(Server.MapPath("~/Content/Images/"), URL));
 
 			model.Foto fotodelete = new data.Foto().GetElement(id);
@@ -74,11 +72,11 @@ namespace VillaBisutti.Delta.WebApp.Controllers
 			new data.Foto().Delete(id);
 
 			return Redirect(Request.UrlReferrer.AbsolutePath);
-        }
+		}
 
-        protected override void Dispose(bool disposing)
-        {
-            base.Dispose(disposing);
-        }
-    }
+		protected override void Dispose(bool disposing)
+		{
+			base.Dispose(disposing);
+		}
+	}
 }
