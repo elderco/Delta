58f3ca085646e61db7ca7336de1830286fff6d4e
leal554 <leal_554@msn.com>
2015-07-22 17:00:30 -0300

    _|_

--- VillaBisutti.Delta/VillaBisutti.Delta.WebApp/Controllers/BoloDoceBemCasadoController.cs
+++ VillaBisutti.Delta/VillaBisutti.Delta.WebApp/Controllers/BoloDoceBemCasadoController.cs
@@ -13,110 +13,46 @@ namespace VillaBisutti.Delta.WebApp.Controllers
 {
     public class BoloDoceBemCasadoController : Controller
     {
+		public ActionResult Index(int id)
+		{
+			ViewBag.Id = id;
+			ViewBag.ContratoAditivoId = new SelectList(new data.ContratoAditivo().GetContratosEvento(id), "Id", "NumeroContrato");
+			ViewBag.TipoItemBoloDoceBemCasadoId = new SelectList(new data.TipoItemBoloDoceBemCasado().GetCollection(0), "Id", "Nome");
+			return View(new data.BoloDoceBemCasado().GetElement(id));
+		}
 
-        // GET: /BoloDoceBemCasado/
-        public ActionResult Index()
-        {
-			return View(new data.BoloDoceBemCasado().GetCollection(0));
-        }
-
-        // GET: /BoloDoceBemCasado/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-			model.BoloDoceBemCasado bolodocebemcasado = new data.BoloDoceBemCasado().GetElement(id.HasValue ? id.Value : 0);
-            if (bolodocebemcasado == null)
-            {
-                return HttpNotFound();
-            }
-            return View(bolodocebemcasado);
-        }
-
-        // GET: /BoloDoceBemCasado/Create
-        public ActionResult Create()
-        {
-			SelectList BoloDoceBemCasado = new SelectList(new data.BoloDoceBemCasado().GetCollection(0).OrderBy(tid => tid.EventoId), "Id", "EventoId");
-			ViewBag.BoloDoceBemCasado = BoloDoceBemCasado;
-			return View();
-        }
-
-        // POST: /BoloDoceBemCasado/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult ItemCreated([Bind(Include="EventoId,Id,Observacoes")] model.BoloDoceBemCasado bolodocebemcasado)
-        {
-            if (ModelState.IsValid)
-            {
-				new data.BoloDoceBemCasado().Insert(bolodocebemcasado);
-                return RedirectToAction("Index");
-            }
-            return View(bolodocebemcasado);
-        }
-
-        // GET: /BoloDoceBemCasado/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-			model.BoloDoceBemCasado bolodocebemcasado = new data.BoloDoceBemCasado().GetElement(id.HasValue ? id.Value : 0);
-            if (bolodocebemcasado == null)
-            {
-                return HttpNotFound();
-            }
-			SelectList BoloDoceBemCasado = new SelectList(new data.BoloDoceBemCasado().GetCollection(0).OrderBy(tid => tid.EventoId), "Id", "EventoId");
-			ViewBag.BoloDoceBemCasado = bolodocebemcasado;
+		public ActionResult Edit(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			model.BoloDoceBemCasado bolodocebemcasado = new data.BoloDoceBemCasado().GetElement(id.Value);
+			if (bolodocebemcasado == null)
+			{
+				return HttpNotFound();
+			}
+			ViewBag.EventoId = id;
 			return View(bolodocebemcasado);
-        }
+		}
 
-        // POST: /BoloDoceBemCasado/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include="EventoId,Id,Observacoes")] model.BoloDoceBemCasado bolodocebemcasado)
-        {
-            if (ModelState.IsValid)
-            {
-				new data.BoloDoceBemCasado().Update(bolodocebemcasado);
-                return RedirectToAction("Index");
-            }
-            return View(bolodocebemcasado);
-        }
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edited([Bind(Include = "EventoId,Id,Observacoes")] model.BoloDoceBemCasado bolodocebemcasado)
+		{
+			new data.BoloDoceBemCasado().Update(bolodocebemcasado);
+			return Redirect(Request.UrlReferrer.AbsolutePath);
+		}
 
-        // GET: /BoloDoceBemCasado/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-			model.BoloDoceBemCasado bolodocebemcasado = new data.BoloDoceBemCasado().GetElement(id.HasValue ? id.Value : 0);
-            if (bolodocebemcasado == null)
-            {
-                return HttpNotFound();
-            }
-            return View(bolodocebemcasado);
-        }
-
-        // POST: /BoloDoceBemCasado/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-			new data.BoloDoceBemCasado().Delete(id);
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            base.Dispose(disposing);
-        }
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+			}
+			base.Dispose(disposing);
+		}
+       
     }
 }
