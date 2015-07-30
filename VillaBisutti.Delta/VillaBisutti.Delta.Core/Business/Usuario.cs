5f72af22516328156909ebbf8300ec5eec119bf9
talesdealmeida <talesdelmeida@gmail.com>
2015-07-29 23:32:16 -0300

    inicio da parada de implantar o perfil.. vai ser muito loko

---
+++ VillaBisutti.Delta/VillaBisutti.Delta.Core/Business/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using model = VillaBisutti.Delta.Core.Model;
+using data = VillaBisutti.Delta.Core.Data;
+
+namespace VillaBisutti.Delta.Core.Business
+{
+    public class Usuario
+    {
+        public static bool GetProfile(model.Usuario usuario, string URL)
+        {
+            //verificar se a URL tem acesso de leitura, total ou não tem acesso
+            
+            return false;
+        }
+    }
+}
